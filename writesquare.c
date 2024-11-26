dbf5104aca22ebad965bc86f33ac0ae260cc1c8c
ben <benebin@protonmail.com>
2024-11-25 17:42:34 -0700

    square wave generator working

--- writesquare.c
+++ writesquare.c
@@ -3,13 +3,18 @@
 #include <stdint.h>
 #include <math.h>
 
-const uint32_t SampleRate = 44100;
-const uint32_t NumSamples = SampleRate*120; //numSamples = samplerate * duration in S
-const uint16_t NumChannels = 1;
-const uint16_t BitsPerSample = 16;
-const uint32_t ByteRate = SampleRate * NumChannels * BitsPerSample/8;
-const uint16_t BlockAlign = NumChannels * BitsPerSample/8;
-const uint32_t SubChunk2Size = NumSamples * NumChannels * BitsPerSample/8;
+#define swapEndian16(x) __builtin_bswap16(x);
+
+
+//CHANGE VARIABLE TYPES
+const int duration = 120;
+const int SampleRate = 44100;
+const long NumSamples = SampleRate*duration; //numSamples = samplerate * duration in S
+const int NumChannels = 1;
+const int BitsPerSample = 16; //(bit depth)
+const long ByteRate = SampleRate * NumChannels * BitsPerSample/8;
+const int BlockAlign = NumChannels * BitsPerSample/8;
+const int SubChunk2Size = NumSamples * NumChannels * BitsPerSample/8;
 
 typedef struct {
 //char is 8 bits/1 byte
@@ -17,7 +22,7 @@ uint8_t chunkID[4]; //BIG ENDIAN 0x52494646 ASCII of "RIFF"
 uint8_t chunkSize[4]; //LITTLE ENDIAN. chunk size is a 4 byte integer, equals size of entire file in bytes minux 8 bytes for ChunkID and ChunkSize
 //chunkSize = 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
 uint8_t format[4]; //BIG ENDIAN. contains letters "WAVE" or in hex 0x57415645
-uint8_t  subChunk1ID[4]; //BIG ENDIAN contains "fmt " or 0x666d7420
+uint8_t subChunk1ID[4]; //BIG ENDIAN contains "fmt " or 0x666d7420
 uint8_t subChunk1Size[4]; //LITTLE ENDIAN. size of rest of subchunk that follows this number
 uint8_t AudioFormat[2]; //PCM = 1. We will only use PCM to represent audio
 uint8_t numChannels[2]; //mono = 1, stereo = 2, etc
@@ -60,26 +65,63 @@ fwrite(wavh.subChunk2Size, sizeof(uint8_t), 4, wavfile);
 
 }
 
-void writeSquare(FILE *wavfile, int frequency) {
-    //start at an offset of 44 bytes (just after the header)
+void writeSquare(FILE *wavfile, int frequency, float amplitude) {
+     //44100hz == 44.1kHz sample rate = 44100 samples per second
+    //432hz square wave = 432 oscillations per second
+    //how many samples per oscillation? 44100/432 = 102
+    int samplesPerOscillation = SampleRate/frequency;
+    //how many oscillations in the whole track? NumSamples/102 
+    int oscillationsPerTrack = NumSamples/samplesPerOscillation;
+   
+   //need to find a way to get these values into 2 element little endian 8 bit int arrays
+    int16_t peak = (int16_t) (INT16_MAX/2)*amplitude; //max height for signed 16 bit int
+    int16_t trough = ~peak +1; //square wave so just negate peak amplitude to get trough
+    
+     //IMPORTANT: samples are 16 bit LITTE ENDIAN, so swap to write correctly 
+    swapEndian16(peak);
+    swapEndian16(trough);
+
+    int8_t peakArr[samplesPerOscillation*2];//each sample is 2 8 bit signed ints in little endian order making 1 16 bit singed int
+    int8_t troughArr [samplesPerOscillation*2]; 
+    //fill oscillation arrays
+   
+    for (int i = 0; i < samplesPerOscillation*2; i++) {
+
+        peakArr[i] = peak;
+    }
+    for (int i = 0; i < samplesPerOscillation*2; i++) {
+        //trough arr[i] = first little endian digit of trough
+        //trough arr[i+1] = second little endian digit of trough
+        troughArr[i] = trough;
+    }
+     //start at an offset of 44 bytes (just after the header)
     fseek(wavfile, 44, SEEK_SET);
     //write a frequency hz (frequency oscillations per second) square wave
-    //one oscillation is just "up, down, up"
+    //one oscillation is just "up, down"
+
+   
+    for (int i = 0; i < oscillationsPerTrack; i++) {
+        //write 1 oscillation
+        fwrite(peakArr, sizeof(int8_t), samplesPerOscillation, wavfile);
+        fwrite(troughArr, sizeof(int8_t), samplesPerOscillation, wavfile);
+     
+    }
+
   
 
 }
 
 void writeNoise(FILE *wavfile) {
-    //start at offset of 44 bytes
+    //start at offset of 44 bytes(just after header)
     fseek(wavfile, 44, SEEK_SET);
-    int sampleAbsMax = 25000;
+    int sampleAbsMax = (INT16_MAX/2);
     //16 bit SIGNED int
     for (int i = 0; i < NumSamples; i++) {
         int16_t sample = rand() % (sampleAbsMax + 1);
         if (i % 2 == 0) {
         fwrite(&sample, sizeof(int16_t), 2, wavfile);
         } else {
-            sample = ~sample +1; //negate sample
+            sample = ~sample +1; //negate sample by 2s comp (~ operator inverts the bits for 1s comp and +1 makes 2s comp)
             fwrite(&sample, sizeof(int16_t), 2, wavfile);
         }
     }
@@ -149,7 +191,9 @@ FILE *wavfile = fopen("sound.wav", "wb");
 
 writeHeader(wavfile, wavh);
 //at this point is the file pointer still pointed to the end of the header??
-writeNoise(wavfile);
+//writeNoise(wavfile);
+
+writeSquare(wavfile, 432, 0.70);
 
 
 
